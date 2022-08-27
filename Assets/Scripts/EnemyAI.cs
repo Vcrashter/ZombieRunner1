c4596c614ddd8d5ae498bf8caa675f6aed2aa23e
Vcrashter <barbu.vladutgeorge@gmail.com>
2022-08-27 15:19:10 +0300

    Enemy follow the player

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Transform target;
+    NavMeshAgent navMeshAgent;
+
     void Start()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SetDestination();
+    }
+
+    void SetDestination()
+    {
+        navMeshAgent.SetDestination(target.position);
     }
 }
