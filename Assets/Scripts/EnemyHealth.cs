926bdab3e612a98234c200fccbc83abb6247156e
Vcrashter <barbu.vladutgeorge@gmail.com>
2022-08-29 23:59:20 +0300

    SHOOT ON ENEMY AND DECREASE HEALTH

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 100f;
+
+    public void TakeDamage(float damage)
+    {
+        hitPoints -= damage;
+        if (hitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    //create a public methode which reduces hitpoints by the amount of damage
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,6 +6,7 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
+    [SerializeField] float damage = 30f;
     
     void Update()
     {
@@ -18,7 +19,14 @@ public class Weapon : MonoBehaviour
     void Shoot()
     {
         RaycastHit hit;
-        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
-        Debug.Log(hit.transform.name + "YES");
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name + "YES");
+            //TODO: ADD SOME HIT EFFECT FOR VISUAL PLAYERS
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damage);
+        }
+        else { return; }
     }
 }
