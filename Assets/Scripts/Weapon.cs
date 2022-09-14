e9847ed42917fa8632319d5f7691e8a86600346b
Vcrashter <barbu.vladutgeorge@gmail.com>
2022-09-15 00:29:39 +0300

    Reduce the ammo when shoot

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -10,19 +10,24 @@ public class Weapon : MonoBehaviour
     [SerializeField] float damage = 30f;
     [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] GameObject hitEffect;
-    
+    [SerializeField] Ammo ammoSlot;
+
     void Update()
     {
         if(Input.GetButtonDown("Fire1"))
         {
-            Shoot();
+           Shoot();
         }
     }
 
     void Shoot()
     {
-        PlayMuzzleFlash();
-        ProcessRaycast();
+        if (ammoSlot.GetCurrentAmmo() > 0)
+        {
+            PlayMuzzleFlash();
+            ProcessRaycast();
+            ammoSlot.ReduceCurrentAmmo();
+        }
     }
 
     private void PlayMuzzleFlash()
