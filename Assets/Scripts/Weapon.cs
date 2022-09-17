40890bf427dbc2d803a04247eb2b255aaf3e54de
Vcrashter <barbu.vladutgeorge@gmail.com>
2022-09-17 13:50:46 +0300

    Different weapons different bullets (2)

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -11,6 +11,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] GameObject hitEffect;
     [SerializeField] Ammo ammoSlot;
+    [SerializeField] AmmoType ammoType;
     [SerializeField] float timeBetweenShots = 0.5f;
 
     bool canShoot = true;
@@ -20,49 +21,49 @@ public class Weapon : MonoBehaviour
         canShoot = true;
     }
 
-    //void Update()
-    //{
-    //    if(Input.GetButtonDown("Fire1") && canShoot == true)
-    //    {
-    //       StartCoroutine(Shoot());
-    //    }
-    //}
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && canShoot == true)
+        {
+            StartCoroutine(Shoot());
+        }
+    }
 
-    //IEnumerator Shoot()
-    //{
-    //    canShoot = false;
-    //    if (ammoSlot.GetCurrentAmmo() > 0)
-    //    {
-    //        PlayMuzzleFlash();
-    //        ProcessRaycast();
-    //        ammoSlot.ReduceCurrentAmmo();
-            
-    //    }
-    //    yield return new WaitForSeconds(timeBetweenShots);
-    //    canShoot = true;
-    //}
+    IEnumerator Shoot()
+    {
+        canShoot = false;
+        if (ammoSlot.GetCurrentAmmo(ammoType) > 0)
+        {
+            PlayMuzzleFlash();
+            ProcessRaycast();
+            ammoSlot.ReduceCurrentAmmo(ammoType);
 
-    //private void PlayMuzzleFlash()
-    //{
-    //    muzzleFlash.Play();
-    //}
+        }
+        yield return new WaitForSeconds(timeBetweenShots);
+        canShoot = true;
+    }
 
-    //private void ProcessRaycast()
-    //{
-    //    RaycastHit hit;
-    //    if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
-    //    {
-    //        CreateHitImpact(hit);
-    //        EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
-    //        if (target == null) return;
-    //        target.TakeDamage(damage);
-    //    }
-    //    else { return; }
-    //}
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
 
-    //private void CreateHitImpact(RaycastHit hit)
-    //{
-    //    GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
-    //    Destroy(impact, 0.1f);
-    //}
+    private void ProcessRaycast()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            CreateHitImpact(hit);
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.TakeDamage(damage);
+        }
+        else { return; }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, 0.1f);
+    }
 }
